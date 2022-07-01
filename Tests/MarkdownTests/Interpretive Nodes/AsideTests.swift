d6cd065a7e4b6c3fad615dcd39890e095a2f63a2
Mic Pringle <94535969+micpringle@users.noreply.github.com>
2022-07-01 10:28:23 +0100

    Update Aside to support custom titles (#61)
    rdar://65856948

--- Tests/MarkdownTests/Interpretive Nodes/AsideTests.swift
+++ Tests/MarkdownTests/Interpretive Nodes/AsideTests.swift
@@ -1,7 +1,7 @@
 /*
  This source file is part of the Swift.org open source project
 
- Copyright (c) 2021 Apple Inc. and the Swift project authors
+ Copyright (c) 2021-2022 Apple Inc. and the Swift project authors
  Licensed under Apache License v2.0 with Runtime Library Exception
 
  See https://swift.org/LICENSE.txt for license information
@@ -43,13 +43,22 @@ class AsideTests: XCTestCase {
         XCTAssertTrue(aside.content[0].root.isIdentical(to: document))
     }
 
-    func testUnknownTag() {
-        let source = "> Hmm: This is something..."
+    func testCustomTag() {
+        let source = "> Hmm: This is something"
         let document = Document(parsing: source)
         let blockQuote = document.child(at: 0) as! BlockQuote
         let aside = Aside(blockQuote)
-        XCTAssertEqual(.note, aside.kind)
-        XCTAssertTrue(aside.content[0].root.isIdentical(to: document))
+        XCTAssertEqual(.init(rawValue: "Hmm")!, aside.kind)
+        
+        // Note that the initial text in the paragraph has been adjusted
+        // to after the tag.
+        let expectedRootDump = """
+            Document
+            └─ BlockQuote
+               └─ Paragraph
+                  └─ Text "This is something"
+            """
+        XCTAssertEqual(expectedRootDump, aside.content[0].root.debugDescription())
     }
 
     func testNoParagraphAtStart() {
@@ -64,13 +73,4 @@ class AsideTests: XCTestCase {
         XCTAssertTrue(aside.content[0].root.isIdentical(to: document))
     }
 
-    func testCaseInsensitive() {
-        for kind in Aside.Kind.allCases {
-            let source = "> \(kind.rawValue.lowercased()): This is a `\(kind.rawValue)` aside."
-            let document = Document(parsing: source)
-            let blockQuote = document.child(at: 0) as! BlockQuote
-            let aside = Aside(blockQuote)
-            XCTAssertEqual(kind, aside.kind)
-        }
-    }
 }
