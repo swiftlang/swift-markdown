cf276932d912f06b4fce7b2b8a69e3f40a15d426
tien.ll <115207438+tienll@users.noreply.github.com>
2022-11-15 17:05:44 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -52,7 +52,7 @@ if ProcessInfo.processInfo.environment["SWIFTCI_USE_LOCAL_DEPS"] == nil {
     // Building standalone, so fetch all dependencies remotely.
     package.dependencies += [
         .package(url: "https://github.com/apple/swift-cmark.git", .branch("gfm")),
-        .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "1.0.1")),
+        .package(url: "https://github.com/apple/swift-argument-parser", .branch("main")),
     ]
 } else {
     // Building in the Swift.org CI system, so rely on local versions of dependencies.
