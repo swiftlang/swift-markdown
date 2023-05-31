0aae479ea46654150816f0947a9531c1ab68091d
NioDeBacker <nio.debacker@student.hogent.be>
2023-05-31 14:12:56 +0200

    add missing header

--- Sources/Markdown/Block Nodes/Block Container Blocks/FootnoteDefinition.swift
+++ Sources/Markdown/Block Nodes/Block Container Blocks/FootnoteDefinition.swift
@@ -1,3 +1,13 @@
+/*
+ This source file is part of the Swift.org open source project
+
+ Copyright (c) 2021 Apple Inc. and the Swift project authors
+ Licensed under Apache License v2.0 with Runtime Library Exception
+
+ See https://swift.org/LICENSE.txt for license information
+ See https://swift.org/CONTRIBUTORS.txt for Swift project authors
+*/
+
 public struct FootnoteDefinition: BlockContainer {
     public var _data: _MarkupData
     init(_ raw: RawMarkup) throws {
