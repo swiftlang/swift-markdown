0b59ad66e3ad094f61e4be9b0c1808bdbaec5239
Kyle <kyle201817146@gmail.com>
2024-02-01 22:58:38 +0800

    Fix Directive argument name and value ranges on non-first line (#79)
    * Remove unnecessary optional check
    
    * Fix and add test case for single line block parse
    
    * Add test for various combinations of directive argument location
    
    * Initialize LineSegment with the text of only one line
    
    Also, update test to verify emoji ranges
    
    * Update documentation for `LineSegment.untrimmedText` to reflect its use
    
    Also deprecate `LineSegment.lineStartIndex`
    Also remove `lineStartIndex` parameter from internal `LineSegment` init
    
    * Remove custom `==(_:_:)` that's same as synthesized implementation
    
    * Update LICENSE year and code format
    
    ---------
    
    Co-authored-by: David Rönnqvist <ronnqvist@apple.com>

--- Sources/Markdown/Block Nodes/Block Container Blocks/BlockDirective.swift
+++ Sources/Markdown/Block Nodes/Block Container Blocks/BlockDirective.swift
@@ -1,7 +1,7 @@
 /*
  This source file is part of the Swift.org open source project
 
- Copyright (c) 2021 Apple Inc. and the Swift project authors
+ Copyright (c) 2021-2023 Apple Inc. and the Swift project authors
  Licensed under Apache License v2.0 with Runtime Library Exception
 
  See https://swift.org/LICENSE.txt for license information
@@ -95,7 +95,6 @@ public extension BlockDirective {
                                                    omittingEmptySubsequences: false).map { lineText -> DirectiveArgumentText.LineSegment in
                                                     let untrimmedText = String(lineText)
                                                     return DirectiveArgumentText.LineSegment(untrimmedText: untrimmedText,
-                                                                                             lineStartIndex: untrimmedText.startIndex,
                                                                                              range: nil)
                                                    } ?? []
         try! self.init(.blockDirective(name: name,
