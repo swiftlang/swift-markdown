eb5b87f6351ec29900b4d1bc09fc7980cf323cd8
Pat Shaughnessy <pat_shaughnessy@apple.com>
2025-04-07 16:08:18 -0700

    Update package dependencies to use the release/6.2 branch

--- Package.swift
+++ Package.swift
@@ -50,7 +50,7 @@ let package = Package(
 if ProcessInfo.processInfo.environment["SWIFTCI_USE_LOCAL_DEPS"] == nil {
     // Building standalone, so fetch all dependencies remotely.
     package.dependencies += [
-        .package(url: "https://github.com/swiftlang/swift-cmark.git", branch: "gfm"),
+        .package(url: "https://github.com/swiftlang/swift-cmark.git", branch: "release/6.2"),
     ]
     
     // SwiftPM command plugins are only supported by Swift version 5.6 and later.
