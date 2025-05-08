bf773dbedf41c666003618ea7ad70523e05260b0
Bina Maniar <bmaniar@apple.com>
2025-05-08 13:52:26 -0700

    Adding support for abstract headerdoc tag so methods and properties with an abstract tag retains its documentation

--- Sources/Markdown/Walker/Walkers/MarkupFormatter.swift
+++ Sources/Markdown/Walker/Walkers/MarkupFormatter.swift
@@ -1,7 +1,7 @@
 /*
  This source file is part of the Swift.org open source project
 
- Copyright (c) 2021-2024 Apple Inc. and the Swift project authors
+ Copyright (c) 2021-2025 Apple Inc. and the Swift project authors
  Licensed under Apache License v2.0 with Runtime Library Exception
 
  See https://swift.org/LICENSE.txt for license information
@@ -1176,6 +1176,11 @@ public struct MarkupFormatter: MarkupWalker {
     public mutating func visitDoxygenDiscussion(_ doxygenDiscussion: DoxygenDiscussion) {
         printDoxygenStart("discussion", for: doxygenDiscussion)
         descendInto(doxygenDiscussion)
+    }
+
+    public mutating func visitDoxygenAbstract(_ doxygenAbstract: DoxygenAbstract) {
+        printDoxygenStart("abstract", for: doxygenAbstract)
+        descendInto(doxygenAbstract)
     }
 
     public mutating func visitDoxygenNote(_ doxygenNote: DoxygenNote) {
